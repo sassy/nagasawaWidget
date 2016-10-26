c55f0ea42877c71c4ec4f9da7b83fdecb478d47a
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-10-27 00:40:24 +0900

    再生できる動画を増やした

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 700, height: 335});
 
   win.loadURL(`file://${__dirname}/index.html`);
 
